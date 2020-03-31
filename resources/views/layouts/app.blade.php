ef2ece64bf521b645863d609dae319ba322ff4e0
mikhael-artur <mikhael.artur.d@gmail.com>
2020-03-31 22:26:29 +0700

    Remove minimized ver.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,18 +104,18 @@
             </nav>
         @endauth
 
-        <div class="main">
+        <div class="main" #main-container>
             <!-- Navbar  -->
             <nav class="navbar navbar-expand navbar-dark sticky-top">
                 <!--Navbar logo-->
                 <div class="navbar-brand mr-0 mr-md-2" href="#">
                     @auth
-                        <button type="button" id="sidebarCollapse" class="btn btn-primary">
-                            <div id="is-hide">
-                                <i class="fas fa-chevron-left toggle-hide"></i>
-                                <i class="fas fa-chevron-right toggle-show"></i>
-                            </div>
-                        </button>
+                        {{--<button type="button" id="sidebarCollapse" class="btn btn-primary" onclick="">--}}
+                            {{--<div id="is-hide">--}}
+                                {{--<i class="fas fa-chevron-left toggle-hide"></i>--}}
+                                {{--<i class="fas fa-chevron-right toggle-show"></i>--}}
+                            {{--</div>--}}
+                        {{--</button>--}}
                     @endauth
                 </div>
 
