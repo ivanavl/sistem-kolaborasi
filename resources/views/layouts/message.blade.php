6b96b94756bd74f22f1c35875ad71ef90bc6be78
mikhael-artur <mikhael.artur.d@gmail.com>
2020-03-31 16:34:32 +0700

    Add success and error message

---
+++ resources/views/layouts/message.blade.php
@@ -0,0 +1,10 @@
+@if(isset($sucess))
+<div class="alert alert-success" role="alert">
+  {{ $success }}
+</div>
+@endif
+@if(isset($error))
+<div class="alert alert-danger" role="alert">
+  {{ $error }}
+</div>
+@endif
